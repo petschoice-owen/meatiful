8b68d227867f3bd8a5ec90016b6ed182502e40c2
petschoice-owen <owen.caisip@petschoice.co.uk>
2022-05-19 10:31:11 +0800

    Updated Product Images

--- functions.php
+++ functions.php
@@ -78,37 +78,6 @@ function theme_scripts()  {
 add_action( 'wp_enqueue_scripts', 'theme_scripts' ); // Register this fxn and allow Wordpress to call it automatcally in the header
 
 
-/*-----------------------------------------------------------------------------------*/
-/* Add Theme Settings Advanced Custom Fields
-/*-----------------------------------------------------------------------------------*/
-if( function_exists('acf_add_options_page') ) {
-	acf_add_options_page(array(
-		'page_title' 	=> 'Theme General Settings',
-		'menu_title'	=> 'Theme Settings',
-		'menu_slug' 	=> 'general-settings',
-		'capability'	=> 'edit_posts',
-		'redirect'		=> false
-	));
-	
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Theme Header',
-		'menu_title'	=> 'Header Section',
-		'parent_slug'	=> 'general-settings',
-	));
-	
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Theme Footer',
-		'menu_title'	=> 'Footer Section',
-		'parent_slug'	=> 'general-settings',
-	));
-	
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Social Media',
-		'menu_title'	=> 'Social Media',
-		'parent_slug'	=> 'general-settings',
-	));
-}
-
 
 /*-----------------------------------------------------------------------------------*/
 /* PHP code for SVG support In WordPress
@@ -141,13 +110,9 @@ function fix_svg() {
 add_action( 'admin_head', 'fix_svg' );
 
 
-
-
-/*
- * -----------------------------------------------------------------------------
- * Advanced Custom Fields Modifications
- * -----------------------------------------------------------------------------
-*/
+/*-----------------------------------------------------------------------------------*/
+/* Advanced Custom Fields Modifications - Resize WYSIWYG Editor
+/*-----------------------------------------------------------------------------------*/
 function PREFIX_apply_acf_modifications() { ?>
 	<style>
 		.acf-editor-wrap iframe {
@@ -173,3 +138,94 @@ function PREFIX_apply_acf_modifications() { ?>
 	</script>
 <?php }
 add_action('acf/input/admin_footer', 'PREFIX_apply_acf_modifications');
+
+
+/*-----------------------------------------------------------------------------------*/
+/* Advanced Custom Fields - Add Theme Settings
+/*-----------------------------------------------------------------------------------*/
+if ( function_exists('acf_add_options_page') ) {
+	acf_add_options_page(array(
+		'page_title' 	=> 'Theme General Settings',
+		'menu_title'	=> 'Theme Settings',
+		'menu_slug' 	=> 'general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+	
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Theme Header',
+		'menu_title'	=> 'Header Section',
+		'parent_slug'	=> 'general-settings',
+	));
+	
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Theme Footer',
+		'menu_title'	=> 'Footer Section',
+		'parent_slug'	=> 'general-settings',
+	));
+	
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Social Media',
+		'menu_title'	=> 'Social Media',
+		'parent_slug'	=> 'general-settings',
+	));
+}
+
+
+/*-----------------------------------------------------------------------------------*/
+/* Register Custom Post Type News
+/*-----------------------------------------------------------------------------------*/
+function create_news_cpt() {
+	$labels = array(
+		'name' => _x( 'News', 'Post Type General Name', 'textdomain' ),
+		'singular_name' => _x( 'News', 'Post Type Singular Name', 'textdomain' ),
+		'menu_name' => _x( 'News', 'Admin Menu text', 'textdomain' ),
+		'name_admin_bar' => _x( 'News', 'Add New on Toolbar', 'textdomain' ),
+		'archives' => __( 'News Archives', 'textdomain' ),
+		'attributes' => __( 'News Attributes', 'textdomain' ),
+		'parent_item_colon' => __( 'Parent News:', 'textdomain' ),
+		'all_items' => __( 'All News', 'textdomain' ),
+		'add_new_item' => __( 'Add New News', 'textdomain' ),
+		'add_new' => __( 'Add New', 'textdomain' ),
+		'new_item' => __( 'New News', 'textdomain' ),
+		'edit_item' => __( 'Edit News', 'textdomain' ),
+		'update_item' => __( 'Update News', 'textdomain' ),
+		'view_item' => __( 'View News', 'textdomain' ),
+		'view_items' => __( 'View News', 'textdomain' ),
+		'search_items' => __( 'Search News', 'textdomain' ),
+		'not_found' => __( 'Not found', 'textdomain' ),
+		'not_found_in_trash' => __( 'Not found in Trash', 'textdomain' ),
+		'featured_image' => __( 'Featured Image', 'textdomain' ),
+		'set_featured_image' => __( 'Set featured image', 'textdomain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'textdomain' ),
+		'use_featured_image' => __( 'Use as featured image', 'textdomain' ),
+		'insert_into_item' => __( 'Insert into News', 'textdomain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this News', 'textdomain' ),
+		'items_list' => __( 'News list', 'textdomain' ),
+		'items_list_navigation' => __( 'News list navigation', 'textdomain' ),
+		'filter_items_list' => __( 'Filter News list', 'textdomain' ),
+	);
+	$args = array(
+		'label' => __( 'News', 'textdomain' ),
+		'description' => __( '', 'textdomain' ),
+		'labels' => $labels,
+		'menu_icon' => 'dashicons-format-aside',
+		'supports' => array('title', 'excerpt', 'thumbnail', 'revisions', 'custom-fields'),
+		'taxonomies' => array(),
+		'public' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'menu_position' => 5,
+		'show_in_admin_bar' => true,
+		'show_in_nav_menus' => true,
+		'can_export' => true,
+		'has_archive' => true,
+		'hierarchical' => false,
+		'exclude_from_search' => false,
+		'show_in_rest' => true,
+		'publicly_queryable' => true,
+		'capability_type' => 'post',
+	);
+	register_post_type( 'news', $args );
+}
+add_action( 'init', 'create_news_cpt', 0 );
