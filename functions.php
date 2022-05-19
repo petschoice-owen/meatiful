b68f644d93896ddf75fd96b783d2154781ab195a
petschoice-owen <owen.caisip@petschoice.co.uk>
2022-05-19 22:49:58 +0800

    add featured image column in admin panel

--- functions.php
+++ functions.php
@@ -241,3 +241,22 @@ if ( function_exists('acf_add_options_page') ) {
 		'parent_slug'	=> 'general-settings',
 	));
 }
+
+
+/*-----------------------------------------------------------------------------------*/
+/* Admin Panel - add Featured Image Column
+/*-----------------------------------------------------------------------------------*/
+add_filter('manage_posts_columns', 'add_img_column');
+add_filter('manage_posts_custom_column', 'manage_img_column', 10, 2);
+
+function add_img_column($columns) {
+    $columns['img'] = 'Featured Image';
+    return $columns;
+}
+
+function manage_img_column($column_name, $post_id) {
+    if( $column_name == 'img' ) {
+        echo get_the_post_thumbnail($post_id, 'thumbnail');
+    }
+    return $column_name;
+}
