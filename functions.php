91d25cdf1f4588a757ed403c01c9e93384285f18
petschoice-owen <owen.caisip@petschoice.co.uk>
2022-07-08 23:00:58 +0800

    Free Shipping Label on Cart and Checkout Page

--- functions.php
+++ functions.php
@@ -347,3 +347,21 @@ function filter_dropdown_option_html( $html, $args ) {
 	$html = str_replace($show_option_none_html, '', $html);
 	return $html;
 }
+
+
+
+/*-----------------------------------------------------------------------------------*/
+/*  Hide shipping rates when free shipping is available.
+/*  Updated to support WooCommerce 2.6 Shipping Zones.
+/*-----------------------------------------------------------------------------------*/
+function my_hide_shipping_when_free_is_available( $rates ) {
+	$free = array();
+	foreach ( $rates as $rate_id => $rate ) {
+		if ( 'free_shipping' === $rate->method_id ) {
+			$free[ $rate_id ] = $rate;
+			break;
+		}
+	}
+	return ! empty( $free ) ? $free : $rates;
+}
+add_filter( 'woocommerce_package_rates', 'my_hide_shipping_when_free_is_available', 100 );
