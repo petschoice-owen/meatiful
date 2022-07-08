8b2e10e752f1ae7fb1c057baead55ddc68afcdd6
petschoice-owen <owen.caisip@petschoice.co.uk>
2022-07-08 17:47:37 +0800

    from dev site

--- woocommerce/single-product/add-to-cart/variation.php
+++ woocommerce/single-product/add-to-cart/variation.php
@@ -14,7 +14,7 @@ defined( 'ABSPATH' ) || exit;
 
 ?>
 <script type="text/template" id="tmpl-variation-template">
-	<div class="woocommerce-variation-description d-none">{{{ data.variation.variation_description }}}</div>
+	<div class="woocommerce-variation-description">{{{ data.variation.variation_description }}}</div>
 	<div class="woocommerce-variation-price d-none" id="woocommerce_variation_price">{{{ data.variation.price_html }}}</div>
 	<!-- <div class="woocommerce-variation-availability">{{{ data.variation.availability_html }}}</div> -->
 </script>
